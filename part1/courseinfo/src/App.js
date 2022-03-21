c5bbc2b212942353ffa915cfc5d60b47158a20c8
Frank Xu <franklolxu@gmail.com>
2022-03-21 17:39:05 -0400

    Finish Exercise 1.1-1.2

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return <p>Number of exercises {props.totalExercise}</p>;
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercise1 = 10;
+  const part2 = "Using props to pass data";
+  const exercise2 = 7;
+  const part3 = "State of a component";
+  const exercise3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercise1={exercise1}
+        part2={part2}
+        exercise2={exercise2}
+        part3={part3}
+        exercise3={exercise3}
+      />
+      <Total totalExercise={exercise1 + exercise2 + exercise3} />
+    </div>
+  );
+};
+
+export default App;
