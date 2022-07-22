847ab78ce9f5d2cb05fa2973f49ffdddc63183fc
Frank Xu <franklolxu@gmail.com>
2022-07-21 21:13:29 -0400

    Finish Exercise 3.18

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,19 +1,26 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const morgan = require('morgan')
+require('dotenv').config()
 const Person = require('./models/person')
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(express.json())
+
+app.use(requestLogger)
 
 app.use(cors())
-app.use(express.json())
-app.use(morgan('tiny'))
+
 app.use(express.static('build'))
 
 app.get('/api/persons', (request, response) => {
-    // response.json(persons)
-    console.log("Getting Persons")
     Person.find({}).then(person => {
         response.json(person)
     })
@@ -26,18 +33,39 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
+    const persons = []
+
+    Person.find({}).then(person => {
+        persons.concat(person)
+    })
+
     const body = `<p>Phonebook has info for ${persons.length} people</p>${new Date()}`
     response.send(body)
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
 app.post('/api/persons', (request, response) => {
-    // console.log("Im here")
     const body = request.body
 
     const persons = []
@@ -45,9 +73,6 @@ app.post('/api/persons', (request, response) => {
     Person.find({}).then(person => {
         persons.concat(person)
     })
-
-    // console.log(persons)
-    // console.log(body)
 
     if (!body.name || !body.number) {
         return response.status(400).json({
@@ -69,6 +94,24 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3005
 
