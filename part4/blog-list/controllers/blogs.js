535649b18393219721b2a472454fd004324d8d40
Frank Xu <franklolxu@gmail.com>
2022-09-12 23:00:55 -0400

    Finish 4.12

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,22 +1,30 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    console.log(blogs);
-    response.json(blogs);
-  });
+blogsRouter.get("/", async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
 });
 
-blogsRouter.post("/", (request, response, next) => {
-  const blog = new Blog(request.body);
+blogsRouter.post("/", async (request, response) => {
+  const body = request.body
 
-  blog
-    .save()
-    .then((savedBlog) => {
-      response.json(savedBlog);
+  if (body.title !== undefined && body.url !== undefined) {
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0
     })
-    .catch((error) => next(error));
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } else {
+    response.status(400).end()
+  }
 });
 
 module.exports = blogsRouter;
---
+++ part4/blog-list/tests/blog_api.test.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const Blog = require('../models/blog')
+const blog = require('../models/blog')
+const api = supertest(app)
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
+
+test('GET: Total number of blogs', async () => {
+    const response = await helper.blogInDb()
+    expect(response).toHaveLength(helper.initialBlogs.length)
+})
+
+test('GET: blogs have ID', async () => {
+    const blogs = await helper.blogInDb()
+    expect(blogs[0].id).toBeDefined()
+}, 100000)
+
+test('POST: Create a new blog', async () => {
+    const blog = {
+        title: 'Test Title',
+        author: 'Test Author',
+        url: 'Test URL',
+        likes: 100
+    }
+    await api.post('/api/blogs').send(blog).expect(201).expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    const titlesAtEnd = blogsAtEnd.map(r => r.title)
+    expect(titlesAtEnd).toContain('Test Title')
+}, 10000)
+
+test('POST: Default Likes = 0', async () => {
+    const blog = {
+        title: 'Test Title',
+        author: 'Test Author',
+        url: 'Test URL',
+    }
+    await api.post('/api/blogs').send(blog).expect(201).expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    const blogAtEnd = blogsAtEnd.filter(blog => blog.title === 'Test Title')
+    expect(blogAtEnd[0].likes).toEqual(0)
+}, 10000)
+
+test('POST: Empty Title and URL', async () => {
+    const blog = {
+        author: 'Test Author',
+    }
+    await api.post('/api/blogs').send(blog).expect(400)
+}, 10000)
+
+afterAll(() => {
+    mongoose.connection.close()
+})
