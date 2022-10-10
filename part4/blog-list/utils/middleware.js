d4bcd574b106f18d57e10aec96cc501bcc0af94d
Frank Xu <franklolxu@gmail.com>
2022-10-10 16:55:40 -0400

    Finish Exercise 4.22

--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require("./logger");
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 const requestLogger = (request, response, next) => {
   logger.info("Method:", request.method);
@@ -15,17 +17,53 @@ const unknownEndpoint = (request, response) => {
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformatted ID' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'Invalid Token'
+    })
   }
 
   next(error);
 };
 
+const tokenExtractor = (request, response, next) => {
+  // logger.info(request)
+  const authorization = request.get('Authorization')
+  if (request.method === 'POST' || request.method === 'DELETE') {
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      request.token = authorization.substring(7)
+    } else {
+      return response.status(401).json({
+        error: 'Invalid Token'
+      })
+    }
+  }
+  next();
+}
+
+const userExtractor = async (request, response, next) => {
+  // logger.info(request.token)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const user = await User.findById(decodedToken.id)
+  if (user) {
+    request.user = user
+    // return request
+  } else {
+    return response.status(401).json({
+      error: 'Invalid User'
+    })
+  }
+  next();
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
+  userExtractor
 };
--- part4/part4-8-notes-backend/app.js
+++ part4/part4-8-notes-backend/app.js
@@ -7,6 +7,7 @@ require('express-async-errors')
 
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/logins')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -27,6 +28,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
