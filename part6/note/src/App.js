20cb0f4e68aa96ca238dea2634db08f149cdb35f
Frank Xu <franklolxu@gmail.com>
2022-11-02 22:06:05 -0400

    Finish Exercise 6.14

--- part6/note/src/App.js
+++ part6/note/src/App.js
@@ -1,8 +1,17 @@
-import Notes from './Notes'
-import NewNote from './NewNote'
-import VisibilityFilter from './components/VIsibilityFilter'
+import Notes from './components/Notes'
+import NewNote from './components/NewNote'
+import VisibilityFilter from './components/VisibilityFilter'
+import { useDispatch } from 'react-redux'
+import noteService from './services/note'
+import { setNotes } from './reducers/noteReducer'
+import { useEffect } from 'react'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    noteService.getAll().then(notes => dispatch(setNotes(notes)))
+  }, [dispatch])
+
   return (
     <div>
       <NewNote />
